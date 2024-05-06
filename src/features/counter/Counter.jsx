99cc255168c077f109c085706d8f64d76f007ec7
amir fattahi <amirfattahi22@gmail.com>
2024-05-06 14:04:30 +0330

    Add files via upload

---
+++ src/features/counter/Counter.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, reset, incrementByAmount } from "./counterSlice";
+import { useState } from "react";
+function Counter() {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState(0);
+  const addValue = Number(incrementAmount) || 0;
+  const resetAll = () => {
+    setIncrementAmount(0);
+    dispatch(reset());
+  };
+  return (
+    <section className="m-5">
+      <p>{count} </p>
+      <button
+        onClick={() => dispatch(increment())}
+        className="bg-rose-500 rounded-full p-2 m-2"
+      >
+        +
+      </button>
+      <button
+        onClick={() => dispatch(decrement())}
+        className="bg-rose-500 rounded-full p-2 m-2"
+      >
+        -
+      </button>
+      <button
+        onClick={() => dispatch(reset())}
+        className="bg-rose-500 rounded-full p-2 m-2"
+      >
+        0
+      </button>
+      <button
+        onClick={() => dispatch(incrementByAmount())}
+        className="bg-rose-500 rounded-full p-2 m-2"
+      >
+        reset all
+      </button>
+      <input
+        type="text"
+        value={incrementAmount}
+        onChange={(e) => setIncrementAmount(e.target.value)}
+      />
+    </section>
+  );
+}
+
+export default Counter;
