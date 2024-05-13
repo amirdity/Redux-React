551e1af9c73878295640b5d0187c13d2aca14188
amir fattahi <amirfattahi22@gmail.com>
2024-05-13 14:56:21 +0330

    typeScript

---
+++ src/state/counter/counterSlice.ts
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface CounterState {
+  value: number;
+}
+const initialState: CounterState = { value: 0 };
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {}
+
+})
+export default counterSlice.reducer;
